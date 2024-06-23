b2f574de66f842d0847c4bd7ca01835dc34233ef
Roberto Colorado <roberuto185@gmail.com>
2024-06-22 23:49:12 -0600

    Se implemento la funcion de actualizar, para el modulo de usuarios

--- Assets/js/functions_usuarios.js
+++ Assets/js/functions_usuarios.js
@@ -60,7 +60,9 @@ document.addEventListener('DOMContentLoaded', function(){
                     formUsuario.reset();
                     swal("Usuarios", objData.msg, "success");
                     tableUsuarios.ajax.reload(function(){
-
+                        fntRolesUsuario();
+                        fntViewUsuario();
+                        fntEditUsuario();
                     });
                 } else {
                     swal("Error", objData.msg, "error");
@@ -73,6 +75,7 @@ document.addEventListener('DOMContentLoaded', function(){
 window.addEventListener('load', function() {
     fntRolesUsuario();
     fntViewUsuario();
+    fntEditUsuario();
 }, false);
 
 function fntRolesUsuario(){
@@ -120,6 +123,49 @@ function fntViewUsuario(){
                         swal("Error", objData.msg, "error");
                     }
                 }
+            }
+        });
+    });
+}
+
+function fntEditUsuario(){
+    var btnEditUsuario = document.querySelectorAll(".btnEditUsuario");
+    btnEditUsuario.forEach(function(btnEditUsuario) {
+        btnEditUsuario.addEventListener('click', function(){
+
+            document.querySelector('#titleModal').innerHTML = "Actualizar Usuario";
+            document.querySelector('.modal-header').classList.replace("headerRegister", "headerUpdate");
+            document.querySelector('#btnActionForm').classList.replace("btn-primary", "btn-info");
+            document.querySelector('#btnText').innerHTML = "Actualizar";
+
+            var idpersona = this.getAttribute("us");
+            var request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+            var ajaxUrl = base_url+'/Usuarios/getUsuario/'+idpersona;
+            request.open("GET",ajaxUrl,true);
+            request.send();
+            request.onreadystatechange = function(){
+                if (request.readyState == 4 && request.status == 200) {
+                    var objData = JSON.parse(request.responseText);
+                    if (objData.status)
+                    {
+                        document.querySelector("#idUsuario").value = objData.data.idpersona;
+                        document.querySelector("#txtIdentificacion").value = objData.data.identificacion;
+                        document.querySelector("#txtNombre").value = objData.data.nombres;
+                        document.querySelector("#txtApellido").value = objData.data.apellidos;
+                        document.querySelector("#txtTelefono").value = objData.data.telefono;
+                        document.querySelector("#txtEmail").value = objData.data.email_user;
+                        document.querySelector("#listRolid").value = objData.data.idrol;
+                        $('#listRolid').selectpicker('render');
+
+                        if (objData.data.status == 1) {
+                            document.querySelector("#listStatus").value = 1;
+                        } else {
+                            document.querySelector("#listStatus").value = 2;
+                        }
+                        $('#listStatus').selectpicker('render');
+                    }
+                }
+                $('#modalFormUsuario').modal('show');
             }
         });
     });
--- Controllers/Usuarios.php
+++ Controllers/Usuarios.php
@@ -42,11 +42,10 @@
 
         public function setUsuario(){
             if ($_POST) {
-
                 if (empty($_POST['txtIdentificacion']) || empty($_POST['txtNombre']) || empty($_POST['txtApellido']) || empty($_POST['txtTelefono']) || empty($_POST['txtEmail']) || empty($_POST['listRolid']) || empty($_POST['listStatus'])) {
                     $arrResponse = array("status" => false, "msg" => 'Datos incorrectos.');
                 } else {
-
+                    $idUsuario = intval($_POST['idUsuario']);
                     $strIdentificacion = strClean($_POST['txtIdentificacion']);
                     $strNombre = ucwords(strClean($_POST['txtNombre']));
                     $strApellido = ucwords(strClean($_POST['txtApellido']));
@@ -55,20 +54,40 @@
                     $intTipoId = intval(strClean($_POST['listRolid']));
                     $intStatus = intval(strClean($_POST['listStatus']));
 
-                    $strPassword = empty($_POST['txtPassword']) ? hash("SHA256",passGenerator()) : hash("SHA256",$_POST['txtPassword']);
+                    if ($idUsuario == 0) {
+                        $option = 1;
+                        $strPassword = empty($_POST['txtPassword']) ? hash("SHA256",passGenerator()) : hash("SHA256",$_POST['txtPassword']);
 
-                    $request_user = $this->model->insertUsuario($strIdentificacion,
-                                                                $strNombre,
-                                                                $strApellido,
-                                                                $intTelefono,
-                                                                $strEmail,
-                                                                $strPassword,
-                                                                $intTipoId,
-                                                                $intStatus);
+                        $request_user = $this->model->insertUsuario($strIdentificacion,
+                                                                    $strNombre,
+                                                                    $strApellido,
+                                                                    $intTelefono,
+                                                                    $strEmail,
+                                                                    $strPassword,
+                                                                    $intTipoId,
+                                                                    $intStatus);
+                    }else{
+                        $option = 2;
+                        $strPassword = empty($_POST['txtPassword']) ? "" : hash("SHA256",$_POST['txtPassword']);
+
+                        $request_user = $this->model->updateUsuario($idUsuario,
+                                                                    $strIdentificacion,
+                                                                    $strNombre,
+                                                                    $strApellido,
+                                                                    $intTelefono,
+                                                                    $strEmail,
+                                                                    $strPassword,
+                                                                    $intTipoId,
+                                                                    $intStatus);
+                    }
 
                     if ($request_user > 0)
                     {
-                        $arrResponse = array("status" => true, "msg" => 'Datos guardados correctamente.');
+                        if ($option == 1) {
+                            $arrResponse = array("status" => true, "msg" => 'Datos guardados correctamente.');
+                        }else{
+                            $arrResponse = array("status" => true, "msg" => 'Datos Actualizados correctamente.');
+                        }
                     }else if($request_user == 'exist')
                     {
                         $arrResponse = array("status" => false, "msg" => '¡Atención! el email o la identificación ya existe, ingrese otro.');
